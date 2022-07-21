b70d345f71919a88b0db790fac3f312443c1b23d
daragmas <daragmas@gmail.com>
2022-07-21 16:30:07 -0400

    commit tree f3be0e8cda5674c06e39950a256d25f9ade9f253

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,90 @@
-import React from "react";
+import React, {useState} from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
 
+
 import { CATEGORIES, TASKS } from "../data";
-console.log("Here's the data you're working with");
-console.log({ CATEGORIES, TASKS });
+// console.log("Here's the data you're working with");
+// console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [tasks, setTasks] = useState(TASKS)
+  const [selectedCategory, setSelectedCategory] = useState('All')
+  
+  const handleNewTask = (newTask) => {
+    setTasks([...tasks, newTask])
+  }
+
+  const handleDeleteTask = (e) => {
+    e.preventDefault()
+    const newTasks = tasks.filter((task)=>task.text !== e.target.id)
+    setTasks(newTasks)
+  }
+  
+  const visibleTasks = tasks.filter((task)=>selectedCategory ==='All' || task.category === selectedCategory)
+
+  const handleCategoryChange = (e)=> {
+    const newCategory = e.target.value
+    setSelectedCategory((selectedCategory) => selectedCategory = newCategory)
+  }
+
+
+
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter 
+        categories={CATEGORIES} 
+        onSelectCategory={handleCategoryChange} 
+        selectedCategory={selectedCategory}/>
+      <NewTaskForm onTaskFormSubmit={handleNewTask} categories={CATEGORIES.filter((category)=>category !== 'All')}/>
+      <TaskList tasks={visibleTasks} removeTask={handleDeleteTask}/>
     </div>
   );
 }
 
 export default App;
+
+
+/*
+{
+    "CATEGORIES": [
+        "All",
+        "Code",
+        "Food",
+        "Money",
+        "Misc"
+    ],
+    "TASKS": [
+        {
+            "text": "Buy rice",
+            "category": "Food"
+        },
+        {
+            "text": "Save a tenner",
+            "category": "Money"
+        },
+        {
+            "text": "Build a todo app",
+            "category": "Code"
+        },
+        {
+            "text": "Build todo API",
+            "category": "Code"
+        },
+        {
+            "text": "Get an ISA",
+            "category": "Money"
+        },
+        {
+            "text": "Cook rice",
+            "category": "Food"
+        },
+        {
+            "text": "Tidy house",
+            "category": "Misc"
+        }
+    ]
+}
+*/
--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,18 @@
 import React from "react";
 
-function CategoryFilter() {
+function CategoryFilter({categories ,onSelectCategory, selectedCategory}) {
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {categories.map((category)=>{return(
+        <button 
+          className={category===selectedCategory? 'selected':null}
+          key={category} 
+          value={category} 
+          onClick={onSelectCategory}>
+            {category}
+          </button>
+      )})}
     </div>
   );
 }
